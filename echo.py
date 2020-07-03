39b0ab1ef377aa5e04d60c05cdedc9ca93b84aa4
dupl10 <dupl10@lzu.edu.cn>
2020-07-03 23:19:42 +0800

    py2 to py3

--- echo.py
+++ echo.py
@@ -1,11 +1,10 @@
-#!/usr/bin/python2
-#coding:utf-8
+#!/home/dupl/anaconda3/bin/python
 def gen(n):
     for i in range(n):
         yield i**2
 
 for i in gen(5):
-    print i
+    print(i)
 
-print "hello world"
-print u"世界,你好"
+print("hello world")
+print("世界,你好")
