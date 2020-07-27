863d53c5a96e8773d19c385e86debd0076a21973
dupl10 <dupl10@lzu.edu.cn>
2020-07-27 20:04:19 +0800

    formated

--- example.py
+++ example.py
@@ -1,8 +1,8 @@
 #!/home/dupl/anaconda3/bin/python
 import itertools
-l=range(1,8)
-it=itertools.permutations(l,7)
-#print(list(it))
+l = range(1, 8)
+it = itertools.permutations(l, 7)
+# print(list(it))
 for i in it:
     if (i[0]*100+8*10+i[1])*i[2] == i[3]*1000+i[4]*100+i[5]*10+i[6]:
         print(i[0]*100+8*10+i[1], i[2], i[3]*1000+i[4]*100+i[5]*10+i[6])
--- f1.py
+++ f1.py
@@ -1,15 +1,15 @@
 #!/home/dupl/anaconda3/bin/python
-f=open('README.md','r')
-l=f.readlines()
+f = open('README.md', 'r')
+l = f.readlines()
 f.close()
-#print l
-n=-1
-l0=[]
+# print l
+n = -1
+l0 = []
 for i in l:
-    j='result('+str(n)+')='+i
+    j = 'result('+str(n)+')='+i
     l0.append(j)
-    n+=1
-#print l0
-f0=open('h1.m','w')
+    n += 1
+# print l0
+f0 = open('h1.m', 'w')
 f0.writelines(l0)
 f0.close()
